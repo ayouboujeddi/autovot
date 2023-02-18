03c8689e7ea307f15a40b19b91e0c7257da9351f
ayouboujeddi <125709639+ayouboujeddi@users.noreply.github.com>
2023-02-18 01:46:27 +0100

    Add files via upload

---
+++ votinbot.py
@@ -0,0 +1,38 @@
+import pyautogui
+import time
+
+position = [(1391, 983),(1247, 361),(1299, 491),(1265, 638),(1336, 790),(1114, 918),(953, 1013)]
+
+time.sleep(5)
+
+
+email = 2142
+
+while True:
+    # time.sleep(1)
+    # print(pyautogui.position())
+    time.sleep(1)
+    pyautogui.click(position[0][0], position[0][1])
+    time.sleep(0.5)
+    pyautogui.click(position[1][0], position[1][1])
+    pyautogui.write('omari')
+    time.sleep(0.5)
+    pyautogui.click(position[2][0], position[2][1])
+    pyautogui.write('adam')
+    time.sleep(0.5)
+    pyautogui.click(position[3][0], position[3][1])
+    pyautogui.write("0676342213")
+    time.sleep(0.5)
+    pyautogui.click(position[4][0], position[4][1])
+    text = "omariadam"+str(email)+"@gmail.com"
+    email+=1
+    print(email)
+    pyautogui.write(text)
+    time.sleep(0.5)
+    pyautogui.click(position[5][0],position[5][1])
+    time.sleep(5)
+    pyautogui.click(position[6][0], position[6][1])
+    
+
+
+
